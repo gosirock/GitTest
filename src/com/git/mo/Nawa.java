d4ff3793ae0a878fe940cd13673ea308e2a1d90a
JoungUkLee <gosirock@naver.com>
2023-04-19 23:40:32 +0900

    addNawa

---
+++ src/com/git/mo/Nawa.java
@@ -0,0 +1,18 @@
+package com.git.mo;
+
+public class Nawa {
+
+	
+	public Nawa() {
+		// TODO Auto-generated constructor stub
+		
+	}
+		
+		
+		public void nam() {
+			System.out.println("hi");
+		}
+	
+		
+		
+}
