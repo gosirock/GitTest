0be2eb0987268ed6f612ecd322078c218ad18a08
JoungUkLee <gosirock@naver.com>
2023-04-19 22:56:01 +0900

    secondRun

--- src/com/git/base/Main.java
+++ src/com/git/base/Main.java
@@ -8,13 +8,10 @@ public class Main {
 		
 		System.out.println("Git Hi and fuck you");
 		
-<<<<<<< HEAD
 		
 		
 		System.out.println("wow");
-=======
 		System.out.println("End");
->>>>>>> 0744a8d14db98ca029826014f5f12ab0369a24d8
 	}
 
 }
