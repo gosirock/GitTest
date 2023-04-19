9778ab2803d9212415a0e4aaf45adff114478dfe
JoungUkLee <gosirock@naver.com>
2023-04-19 23:46:31 +0900

    NewDate

--- src/com/git/mo/NewDate.java
+++ src/com/git/mo/NewDate.java
@@ -2,4 +2,13 @@ package com.git.mo;
 
 public class NewDate {
 
+	
+	public NewDate() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public void oldDate() {
+		System.out.println("old");
+	}
 }
