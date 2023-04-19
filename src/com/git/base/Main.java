cc37102aec1e9812a54df637bb5ea72a0935718e
JoungUkLee <gosirock@naver.com>
2023-04-19 22:35:48 +0900

    First_Update

---
+++ src/com/git/base/Main.java
@@ -0,0 +1,12 @@
+package com.git.base;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Git Hi and fuck you");
+	}
+
+}
